2b4784fb68d0c60ab19ad70da0eef56df7bd9dd6
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2022-06-23 16:59:14 +0500

    feat: create connection sockets

--- index.js
+++ index.js
@@ -7,3 +7,42 @@ const HTTP_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({port:8080});
+
+wss.on('connection', ws=>{
+
+  ws.on('message', data=>{
+    const comands = data.toString().split(' ');
+    const comand = comands[0];
+    const comandInfo = comands[1];
+    // ws.send(comand[0]);
+    if(comand === 'mouse_up'){
+      ws.send(comand)
+    }else if(comand === 'mouse_left'){
+      ws.send(comand)
+    }else if(comand === 'mouse_right'){
+      ws.send(comand)
+    }else if(comand === 'mouse_down'){
+      ws.send(comand)
+    }else if(comand === 'mouse_position'){
+      ws.send(`${comand}`);
+    }else if(comand === 'draw_circle'){
+      ws.send(`${comand}`);
+    }else if(comand === 'draw_rectangle'){
+      ws.send(`${comand}`);
+    }else if(comand === 'draw_square'){
+      ws.send(`${comand}`);
+    }else if(comand === 'prnt_scrn'){
+      ws.send(`${comand}`);
+    }
+    
+    
+    
+    console.log(comand, comandInfo);
+  });
+
+  ws.on('close', () => {
+    console.log(`Client is closed`);
+})
+})
